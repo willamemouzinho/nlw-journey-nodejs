d292f30565b6bc67d6e02c450d2ba0b364b1332d
Willame Mouzinho <willame.dev@gmail.com>
2024-07-08 22:37:03 -0300

    feat: add prisma client

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
+import { createTrip } from './routes/create-trip'
 
 async function main() {
   const server = fastify({ logger: true })
 
   // configs
-
-  // middlewares
-  // server.decorate('authenticate', ensureAuthenticated)
+  server.register(fastifyCors, {
+    origin: 'http://127.0.0.1',
+  })
 
   // routes
-  // server.register(routes, { prefix: '/api/v3' })
+  server.register(createTrip, { prefix: '/api/v1' })
 
   // handle errors
   server.setErrorHandler((error, _req, reply) => {
